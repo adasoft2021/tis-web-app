bc799575115eca9aaefec6da5280be6dd064e298
Luis Tapia <luistapia2998@gmail.com>
2021-10-01 17:47:34 -0400

    TIS9 Cargando un archivo localmente

---
+++ src/pages/board/Board.jsx
@@ -0,0 +1,26 @@
+import Page from '../Page'
+
+export default function Board() {
+	const offer = {
+		name: 'ADASOFT',
+		file: '/files/ADASOFTParteA.pdf',
+	}
+
+	const openInNewTab = (url) => {
+		const newWindow = window.open(url)
+		if (newWindow) newWindow.opener = null
+		console.log(url)
+	}
+
+	return (
+		<Page>
+			<a
+				onClick={() =>
+					openInNewTab(process.env.PUBLIC_URL + offer.file)
+				}
+			>
+				<button>VER</button>
+			</a>
+		</Page>
+	)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import HomePage from './home'
 import NotFoundPage from './NotFoundPage'
+import Board from './board'
 
-export { HomePage, NotFoundPage }
+export { HomePage, NotFoundPage, Board }
