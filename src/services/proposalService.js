f4b22cd5412a80be86a069caae2b5375b2e7e013
jesusjimeneztapia <jesusjimeneztapia456@gmail.com>
2021-10-04 17:27:29 -0400

    Creación del servicio para el listado de propuestas.

---
+++ src/services/proposalService.js
@@ -0,0 +1,19 @@
+const { default: createInstance } = require('./http')
+
+const proposalService = createInstance({ url: '/proposals' })
+
+export async function getAllAdviserProposals({
+	adviserId = 1,
+	adviserToken = 'eyJhbCI6IkhTMjU2IiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJpZCI6IjEiLCJ1c2VybmFtZSI6ImxldGljaWEiLCJpYXQiOjE1MTYyMzkwMjJ9.erG1UFNeP9IjSl7ZuZRwDjo32yLsSYQUbULJ8LGq2cQ',
+}) {
+	const response = await proposalService.get('', {
+		headers: {
+			Authorization: adviserToken,
+		},
+		params: {
+			adviser: adviserId,
+		},
+	})
+
+	return response.data
+}
