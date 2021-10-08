8b56152216a39658f5cac8da58a707bd8fb0e3b3
jesusjimeneztapia <jesusjimeneztapia456@gmail.com>
2021-10-08 17:53:30 -0400

    Crear la funcionalidad para obtener un Review por su ID.

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -6,3 +6,8 @@ export async function createReview(reviewDTO) {
 	const response = await reviewService.post('', reviewDTO)
 	return response.data
 }
+
+export async function getReview(reviewId) {
+	const response = await reviewService.get(`/${reviewId}`)
+	return response.data
+}
