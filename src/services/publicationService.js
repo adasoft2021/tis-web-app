0ad95840c6cf2dddcc9ace61fea80ce820699dfc
jesusjimeneztapia <jesusjimeneztapia456@gmail.com>
2021-10-13 18:19:17 -0400

    TIS-94 Crear servicio para obtener todas las publicaciones por ID del Asesor.

--- src/services/publicationService.js
+++ src/services/publicationService.js
@@ -1,9 +1,31 @@
 import createInstance from './http'
 
+export const PUBLICATION_TYPE = {
+	ANNOUNCEMENT: 'ANNOUNCEMENT',
+	SPECIFICATION_SHEET: 'SPECIFICATION_SHEET',
+}
+
 const publicationService = createInstance({ url: '/publications' })
 
 export async function updatePublication({ publicationId, publicationDTO }) {
-	const response = publicationService.put(`/${publicationId}`, publicationDTO)
+	const response = await publicationService.put(
+		`/${publicationId}`,
+		publicationDTO
+	)
+
+	return response.data
+}
+
+export async function getAllAdviserPublications({
+	adviserId,
+	publicationType,
+}) {
+	const response = await publicationService.get('', {
+		params: {
+			adviserId,
+			type: publicationType,
+		},
+	})
 
 	return response.data
 }
