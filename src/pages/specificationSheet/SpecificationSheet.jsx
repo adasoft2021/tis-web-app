f1d9819b30f1806247248e10d3d87f41b53a8430
VioletaBG <viobel998@gmail.com>
2021-10-16 19:30:37 -0400

    TIS-99 Se reutilizo la vista de la lista del pliego de especificaciones

--- src/pages/specificationSheet/SpecificationSheet.jsx
+++ src/pages/specificationSheet/SpecificationSheet.jsx
@@ -2,14 +2,14 @@ import PublicationView from '../../components/PublicationView'
 import { PublicationProvider } from '../../context/providers/PublicationContext'
 import Page from '../Page'
 
-export default function Announcement() {
+export default function SpecificationSheet() {
 	return (
 		<Page>
 			<PublicationProvider>
 				<PublicationView
-					title='Convocatorias'
-					message='No existen convocatorias'
-					buttonMessage='Nueva convocatoria'
+					title='Publicacion de especificaciones'
+					message='No existen pliegos de especificaciones'
+					buttonMessage='Nuevo pliego de especificaciones'
 				/>
 			</PublicationProvider>
 		</Page>
