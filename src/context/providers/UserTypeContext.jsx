5741cc6a4254e926c5497ad4e199b79506a9e660
TapiaX <luistapia2998@gmail.com>
2021-10-22 08:59:15 -0400

    Update UserTypeContext.jsx

--- src/context/providers/UserTypeContext.jsx
+++ src/context/providers/UserTypeContext.jsx
@@ -3,16 +3,17 @@ import { createContext, useContext } from 'react'
 export const userTypes = {
 	ADVISER: 'ADVISER',
 	COMPANY: 'COMPANY',
+	ANONYMOUS: 'ANONYMOUS',
 }
-export const initialState = { userType: userTypes.ADVISER }
-const UserTypeContext = createContext(...initialState)
+export const initialState = { userType: userTypes.ANONYMOUS }
+const UserTypeContext = createContext()
 export const useUserType = () => {
 	const context = useContext(UserTypeContext)
 	return context
 }
-export const UserTypeProvider = ({ state, children }) => {
+export const UserTypeProvider = ({ children }) => {
 	return (
-		<UserTypeContext.Provider value={state}>
+		<UserTypeContext.Provider>
 			{children}
 		</UserTypeContext.Provider>
 	)
