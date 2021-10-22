5ffbc1a31c19c31623752b334af088273f78ec97
Leonardo-D-R-F <leonardo.roldan1139@gmail.com>
2021-10-22 12:19:47 -0400

    Funcionando  firebase y formik

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useFormik } from 'formik'
 import { Modal, Row, Button, Form, InputGroup } from 'react-bootstrap'
 import Styles from './NewPost.module.scss'
@@ -5,93 +6,21 @@ import { useToast } from '../context/providers/ToastContext'
 import * as Yup from 'yup'
 import swal from 'sweetalert'
 import { app } from '../fb'
-import { useState } from 'react'
 // import DatePicker from 'react-datepicker'
-
+const getToDay = () => {
+	const ToDay = new Date()
+	const now = `${ToDay.getUTCFullYear()}-${
+		ToDay.getUTCMonth() + 1
+	}-${ToDay.getUTCDate()}`
+	return now
+}
 const SignupForm = ({ show, onHide, header, fun, semester }) => {
-	const getToDay = () => {
-		const ToDay = new Date()
-		const now = `${ToDay.getUTCFullYear()}-${
-			ToDay.getUTCMonth() + 1
-		}-${ToDay.getUTCDate()}`
-		return now
-	}
-
-	const { showToast } = useToast()
-
-	const validationPDF = async (e) => {
-		if (e.target.files[0].type !== 'application/pdf') {
-			showToast({
-				color: 'danger',
-				message: 'No es un archivo PDF',
-			})
-			changeFile(e)
-		} else {
-			showToast({
-				color: 'success',
-				message: 'El archivo PDF se ha cargado correctamente',
-			})
-		}
-	}
-
-	const [fileUrl, setFileUrl] = useState('')
-
-	const changeFile = (e) => {
-		const reader = new FileReader()
-		reader.onload = (e) => {
-			e.preventDefault()
-			// setFile(e.target.result)
-		}
-	}
-
-	const uploadFile = async (e) => {
-		const file = e.target.files[0]
-		const storageRef = app.storage().ref()
-		const filePath = storageRef.child(file.name)
-		try {
-			await filePath.put(file)
-			const fileDownloadUrl = await filePath.getDownloadURL()
-			setFileUrl(fileDownloadUrl)
-			formik.setFieldValue('attachedfile', fileDownloadUrl)
-			console.log(
-				'archivo cargado: %s\n url:%s',
-				file.name,
-				fileDownloadUrl
-			)
-		} catch {
-			showToast({
-				color: 'danger',
-				message: 'No se pudo subir el archivo',
-			})
-		}
-	}
-
-	const saveUrl = async (e) => {
-		e.preventDefault()
-		const fileName = e.target.PDF.files[0].name
-		if (!fileName || !fileUrl) {
-			showToast({
-				color: 'danger',
-				message: !fileName
-					? 'filename vacio'
-					: '' + !fileUrl
-					? ' url vacia'
-					: '',
-			})
-			return
-		}
-		const collectionRef = app.firestore().collection('publicationFiles')
-		await collectionRef
-			.doc(fileName)
-			.set({ fileName: fileName, url: fileUrl })
-		console.log('url guardada:', fileName, 'ulr:', fileUrl)
-	}
-
 	const formik = useFormik({
 		initialValues: {
 			title: '',
 			date: '',
 			code: '',
+			semester: '',
 			attachedfile: '',
 		},
 		validationSchema: Yup.object({
@@ -116,16 +45,50 @@ const SignupForm = ({ show, onHide, header, fun, semester }) => {
 					/^[12]{1}[-][0-9]{4}$/,
 					'Por favor siga el siguiente formato Ej: (1-2021)'
 				),
+			attachedfile: Yup.string().required(
+				'Es necesario subir un archivo para continuar'
+			),
 		}),
 
 		onSubmit: (values) => {
-			uploadFile()
-			saveUrl(values.title)
-			alert(JSON.stringify({ ...values, attachedfile: fileUrl }, null, 2))
-			fun()
+			alert(JSON.stringify(values, null, 2))
 		},
 	})
 
+	const { showToast } = useToast()
+	const validationPDF = async (e) => {
+		if (e.target.files[0].type !== 'application/pdf') {
+			showToast({
+				color: 'danger',
+				message: 'No es un archivo PDF',
+			})
+		} else {
+			showToast({
+				color: 'success',
+				message: 'El archivo PDF se ha cargado correctamente',
+			})
+			uploadFile(e)
+		}
+	}
+	const uploadFile = async (e) => {
+		const file = e.target.files[0]
+		const storageRef = app.storage().ref()
+		const filePath = storageRef.child(file.name)
+		try {
+			await filePath.put(file)
+			const fileDownloadUrl = await filePath.getDownloadURL()
+			// setFileUrl(fileDownloadUrl)
+			formik.setFieldValue('attachedfile', fileDownloadUrl)
+			console.log(formik.values.attachedfile)
+		} catch {
+			showToast({
+				color: 'danger',
+				message: 'No se pudo subir el archivo',
+			})
+		}
+	}
+
+	// const { showToast } = useToast()
 	return (
 		<Modal
 			show={show}
@@ -134,15 +97,7 @@ const SignupForm = ({ show, onHide, header, fun, semester }) => {
 			aria-labelledby='contained-modal-title-vcenter'
 			centered
 			backdrop='static'
-			keyboard={false}
 		>
-			{' '}
-			<Form onSubmit={saveUrl}>
-				<Form.Group controlId='PDF'>
-					<Form.Control type='file' onChange={uploadFile} />
-					<Button type='submit'>Enviar </Button>
-				</Form.Group>
-			</Form>
 			<Form className='bg-dark' onSubmit={formik.handleSubmit} noValidate>
 				<div className='m-5'>
 					<div className=' d-flex justify-content-end'>
@@ -230,6 +185,9 @@ const SignupForm = ({ show, onHide, header, fun, semester }) => {
 										disabled={true}
 										title='Este campo es llenado por defecto para el semestre actual'
 									/>
+									<Form.Control.Feedback type='invalid'>
+										{formik.errors.semester}
+									</Form.Control.Feedback>
 								</InputGroup>
 							</Form.Group>
 							<div className='mt-3 mb-3'>
@@ -239,7 +197,7 @@ const SignupForm = ({ show, onHide, header, fun, semester }) => {
 								<Row
 									className={`${Styles['drag-area']} rounded bg-light text-dark p-4`}
 								>
-									<Form.Group controlId='PDF'>
+									<Form.Group controlId='PDF' hasValidation>
 										<div className='d-flex flex-column align-items-center'>
 											<h4>
 												Arrastra y suelta tu archivo
@@ -249,14 +207,26 @@ const SignupForm = ({ show, onHide, header, fun, semester }) => {
 												Selecciona tu archivo
 											</Form.Label>
 										</div>
-										<Form.Control
-											className={
-												Styles['file-upload-input']
-											}
-											type='file'
-											accept='application/pdf'
-											onChange={validationPDF}
-										/>
+										<InputGroup>
+											<Form.Control
+												className={
+													Styles['file-upload-input']
+												}
+												type='file'
+												accept='application/pdf'
+												onChange={(e) => {
+													validationPDF(e)
+												}}
+												isInvalid={
+													formik.touched
+														.attachedfile &&
+													formik.errors.attachedfile
+												}
+											/>
+										</InputGroup>
+										<Form.Control.Feedback type='invalid'>
+											{formik.errors.attachedfile}
+										</Form.Control.Feedback>
 									</Form.Group>
 								</Row>
 							</div>
