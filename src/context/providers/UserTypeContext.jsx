918ed3362760b2a7c8f8656326dd2fefb58a6eab
TapiaX <luistapia2998@gmail.com>
2021-10-22 18:46:44 -0400

    Creando el contexto para UserType con null

--- src/context/providers/UserTypeContext.jsx
+++ src/context/providers/UserTypeContext.jsx
@@ -3,10 +3,9 @@ import { createContext, useContext } from 'react'
 export const userTypes = {
 	ADVISER: 'ADVISER',
 	COMPANY: 'COMPANY',
-	ANONYMOUS: 'ANONYMOUS',
 }
-export const initialState = { userType: userTypes.ANONYMOUS }
-const UserTypeContext = createContext()
+export const initialState = { userType: null }
+const UserTypeContext = createContext(initialState)
 export const useUserType = () => {
 	const context = useContext(UserTypeContext)
 	return context
