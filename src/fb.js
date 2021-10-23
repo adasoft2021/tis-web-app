634e6a6f13ec90e99e048a5a8656edba084ef18b
Luis Tapia <luistapia2998@gmail.com>
2021-10-23 19:14:42 -0400

    Publication firebase pasar variables de entorno por javascript

--- src/fb.js
+++ src/fb.js
@@ -1,14 +1,23 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/storage'
 import 'firebase/compat/firestore'
-
+const {
+	FIREBASE_PROJECT_ID,
+	FIREBASE_APP_ID,
+	FIREBASE_STORAGE_BUCKET,
+	FIREBASE_LOCATION_ID,
+	FIREBASE_API_KEY,
+	FIREBASE_AUTH_DOMAIN,
+	FIREBASE_MESSAGING_SENDER_ID,
+	FIREBASE_MEASUREMENT_ID,
+} = require('./config/environment')
 export const app = firebase.initializeApp({
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	locationId: process.env.REACT_APP_FIREBASE_LOCATION_ID,
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+	projectId: FIREBASE_PROJECT_ID,
+	appId: FIREBASE_APP_ID,
+	storageBucket: FIREBASE_STORAGE_BUCKET,
+	locationId: FIREBASE_LOCATION_ID,
+	apiKey: FIREBASE_API_KEY,
+	authDomain: FIREBASE_AUTH_DOMAIN,
+	messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+	measurementId: FIREBASE_MEASUREMENT_ID,
 })
