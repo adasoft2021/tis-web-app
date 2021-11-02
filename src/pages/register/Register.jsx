a6055c7d048d7d2648e4da6cd53f34779801d1a3
Leonardo Dennis Roldan Fernandez <leonardo.roldan1139@gmail.com>
2021-11-02 11:24:26 -0400

    TIS-31 componente registro de G.E page

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CGregistrationForm from '../../components/CGregistrationForm'
+import Page from '../Page'
+export default function Register() {
+	return (
+		<Page>
+			<CGregistrationForm />
+		</Page>
+	)
+}
