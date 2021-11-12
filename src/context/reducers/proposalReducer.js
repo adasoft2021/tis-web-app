fe93e9ce3d96e99f1c9d9c9b93790e0c2eef59b6
jesusjimeneztapia <jesusjimeneztapia456@gmail.com>
2021-11-11 20:57:33 -0400

    Arreglando lista de Propuestas.

--- src/context/reducers/proposalReducer.js
+++ src/context/reducers/proposalReducer.js
@@ -1,6 +1,7 @@
 import { PROPOSAL_ACTIONS } from '../actions/proposalActions'
 
 export const proposalInitialState = {
+	proposals: [],
 	proposal: null,
 	error: null,
 	isLoading: false,
@@ -8,6 +9,23 @@ export const proposalInitialState = {
 
 export const proposalReducer = (state, { type, payload }) => {
 	switch (type) {
+		case PROPOSAL_ACTIONS.LOAD_LIST_PROPOSALS:
+			return {
+				...state,
+				isLoading: true,
+			}
+		case PROPOSAL_ACTIONS.LOAD_LIST_PROPOSALS_SUCCESS:
+			return {
+				...state,
+				proposals: payload,
+				error: null,
+				isLoading: false,
+			}
+		case PROPOSAL_ACTIONS.LOAD_LIST_PROPOSALS_ERROR:
+			return {
+				...state,
+				error: payload,
+			}
 		case PROPOSAL_ACTIONS.LOAD_GET_PROPOSAL:
 			return {
 				...state,
