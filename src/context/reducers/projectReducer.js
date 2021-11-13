0506d274a0b75280a629e3e2eee624cb50561c0f
jesusjimeneztapia <jesusjimeneztapia456@gmail.com>
2021-11-12 21:41:41 -0400

    Creacion del Provider de Proyecto e implementacion para crear un Proyecto.

--- src/context/reducers/projectReducer.js
+++ src/context/reducers/projectReducer.js
@@ -2,26 +2,14 @@ import { PROJECT_ACTIONS } from '../actions/projectActions'
 
 export const projectInitialState = {
 	projects: [],
-	isLoading: false,
 }
 
 export const projectReducer = (state, { type, payload }) => {
 	switch (type) {
-		case PROJECT_ACTIONS.LOAD_CREATE_PROJECT:
-			return {
-				...state,
-				isLoading: true,
-			}
-		case PROJECT_ACTIONS.LOAD_CREATE_PROJECT_SUCCESS:
+		case PROJECT_ACTIONS.ADD_CREATED_PROJECT:
 			return {
 				...state,
 				projects: [...state.projects, payload],
-				isLoading: false,
-			}
-		case PROJECT_ACTIONS.STOP_LOADING:
-			return {
-				...state,
-				isLoading: true,
 			}
 	}
 }
